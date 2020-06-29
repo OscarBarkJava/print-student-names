8d6cc38436f0de698b259070d3a874cb02b0a86f
OscarBarkJava <bark_throwaway@protonmail.com>
2020-06-29 15:24:53 +0200

    Add placeholder Java code with basic program for printing student names

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	static final String[] STUDENT_NAMES = 
+		{"Andre as",
+		 "Ocsar",
+		 "Annna",
+		 "Petr4"};
+	
+	public static void main(String[] args) {
+		for (String name : STUDENT_NAMES) {
+			System.out.println(name);
+		}
+	}
+
+}
